2e933d419ccfbd4e55534f20570fee4984a0494a
plaaaaaton <fancaran@gmail.com>
2022-02-11 16:01:57 +0300

    storage shutdown added

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-
 from db import Database
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -15,37 +14,49 @@ db = Database('hse_curator.db')
 name = ''
 age = -1
 
+
 class Form(StatesGroup):
     name = State()
+
 
 # Проверка на существование в бд, регистрация (просто имя)
 @dp.message_handler(commands='reg')
 async def get_text_messages(message):
-    if db.user_exitst(message.from_user.username):
+    if db.user_exists(message.from_user.username):
         await bot.send_message(message.from_user.id, 'Ты уже зарегистрирован!')
     else:
         await Form.name.set()
         await bot.send_message(message.from_user.id, 'Как тебя зовут?')
 
+
 # Регистрируем имя
-@dp.message_handler(state= Form.name)
-async def process_name(message: types.message, state:FSMContext):
+@dp.message_handler(state=Form.name)
+async def process_name(message: types.message, state: FSMContext):
     name = message.text
     await state.finish()
     print(message.from_user.username)
     db.add_user(str(message.from_user.username), name)
     await bot.send_message(message.from_user.id, 'Ты зарегистрирован!')
 
+
 # Обработка обращения к менюшке, потом сделаем
 @dp.message_handler(commands=['menu'])
 async def show_menu(message: types.message):
     await message.reply('меню')
 
+
 # Обработка обращения к профилю, потом сделаем
 @dp.message_handler(commands=['profile'])
 async def show_profile(message: types.message):
     await bot.send_message(message.from_user.id, 'Твой профиль:')
-    await bot.send_message(message.from_user.id, db.get_name(message.from_user.username) + '\nВсего очков: ' + str(db.get_points(message.from_user.username)))
+    await bot.send_message(message.from_user.id, db.get_name(message.from_user.username) + '\nВсего очков: ' + str(
+        db.get_points(message.from_user.username)))
+
+
+async def shutdown(dispatcher: Dispatcher):
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
+
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, on_shutdown=shutdown())
--- db.py
+++ db.py
@@ -1,12 +1,16 @@
 import sqlite3
+
+
 class Database:
     def __init__(self, db_file):
         self.connection = sqlite3.connect(db_file)
         self.cursor = self.connection.cursor()
-    def user_exitst(self, user_id):
+
+    def user_exists(self, user_id):
         with self.connection:
             result = self.cursor.execute('SELECT * FROM users WHERE tg_username=?', [str(user_id)]).fetchmany(1)
             return bool(len(result))
+
     def add_user(self, user_id, user_name):
         with self.connection:
             return self.cursor.execute('INSERT INTO users (tg_username, person_name, points, answered_questions_id) '
@@ -14,9 +18,12 @@ class Database:
 
     def get_points(self, user_id):
         with self.connection:
-            result = self.cursor.execute('SELECT points FROM users WHERE tg_username= \"' + str(user_id) + '\"').fetchone()[0]
+            result = \
+                self.cursor.execute('SELECT points FROM users WHERE tg_username= \"' + str(user_id) + '\"').fetchone()[
+                    0]
             return str(result)
 
     def get_name(self, user_id):
         with self.connection:
-            return str(self.cursor.execute('SELECT person_name FROM users WHERE tg_username = \"' + str(user_id) + '\"').fetchone()[0])
+            return str(self.cursor.execute(
+                'SELECT person_name FROM users WHERE tg_username = \"' + str(user_id) + '\"').fetchone()[0])
